b3d635e73d03c964a99d8fd4d7a62268cc5913f9
Bertrand Néron <bneron@pasteur.fr>
2014-11-21 19:02:22 +0100

    trsform code of fibonacci in function

--- source/_static/code/fibonacci_iteration.py
+++ source/_static/code/fibonacci_iteration.py
@@ -1,13 +1,18 @@
 
-fib_suite = []
-n = 0
-while n < 10:
-    if n == 0:
-        fib_suite.append(0)
-    elif n == 1:
-        fib_suite.append(1)
-    else: 
-        res = fib_suite[n-1] + fib_suite[n-2] 
-        fib_suite.append(res)
-    n += 1
-print ', '.join([str(i) for i in fib_suite])
+def fibonacci(n):
+    fib_suite = []
+    i = 0
+    while i <= n:
+        if i == 0:
+            fib_suite.append(0)
+        elif i == 1:
+            fib_suite.append(1)
+        else: 
+            res = fib_suite[i-1] + fib_suite[i-2] 
+            fib_suite.append(res)
+        i += 1
+    return fib_suite
+
+print ', '.join([str(i) for i in fibonacci(10)])
+
+
